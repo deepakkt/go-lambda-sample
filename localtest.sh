1b2808ea2b34d77ba33a33824c065434a8696049
Deepak <deepak.kumaran@aurea.com>
2021-08-05 15:53:29 +0530

    update local test scripts

--- localtest.sh
+++ localtest.sh
@@ -1,15 +1,25 @@
+#!/usr/bin/env bash
+## Script to execute the lambda locally
 
+## INSTALL AWS Lambda Runtime Simulator
 mkdir -p ~/.aws-lambda-rie \
 && curl -Lo ~/.aws-lambda-rie/aws-lambda-rie \
 https://github.com/aws/aws-lambda-runtime-interface-emulator/releases/latest/download/aws-lambda-rie \
 && chmod +x ~/.aws-lambda-rie/aws-lambda-rie
 
+# Fetch last commit from local Git repo
 GIT_LAST_COMMIT=$(git rev-parse --short HEAD)
 
-docker build -t deployment-notifications:${GIT_LAST_COMMIT} .
+docker build -t deployment-notifications:"${GIT_LAST_COMMIT}" .
 
+## Execute image
+## Note: Update env-file with live AWS creds from SSO session
+## change other params as suitable for testing
 docker run -d -v ~/.aws-lambda-rie:/aws-lambda \
---env-file ./env-file \
+--env-file ./local-test/env-file \
 --entrypoint /aws-lambda/aws-lambda-rie  \
--p 9000:8080 deployment-notifications:${GIT_LAST_COMMIT} /main
+-p 9000:8080 deployment-notifications:"${GIT_LAST_COMMIT}" /main
+
+## Sample local execution (this is like a lambda execution)
+curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d @local-test/ecsevent.txt
 
